5820fa97e83065eced369f6459fabc597c3a765e
DuDe <dumontderek@gmail.com>
2016-03-16 11:15:43 +0100

    Fin exo HTML/CSS/JS nombre Magik

--- JSCRIPT/NombreMagic8.js
+++ JSCRIPT/NombreMagic8.js
@@ -1,6 +1,6 @@
 var btn1 = document.querySelector("button");
 var btn2 = document.querySelector("#btn2");
-
+var lbl1 = document.querySelector("#lbl1");
 var textBox1 = document.querySelector("input");
 
  
@@ -10,21 +10,47 @@ var textBox1 = document.querySelector("input");
 btn1.addEventListener("click", function ()
 {
 
- if (textBox1.value < NM)
-    {
-        alert("Plus grand... Réessayez");                   
-    }
+	if (textBox1.value < NM)
+	    {
+	        lbl1.innerHTML = "Plus grand... Réessayez";
+	        textBox1.value = "";
+			textBox1.focus();                   
+			lbl1.style.color="white";
+			setTimeout (function()
+			{
+				lbl1.style.color="#607D8B";
+			 	console.log("interval");
+			}, 2000);
+	    }
 
-else if (textBox1.value > NM)
-    {
-        alert("Plus petit...  Réessayez");
-    }
- 
+	else if (textBox1.value > NM)
+	    {
+	        lbl1.innerHTML = "Plus petit...  Réessayez";
+	        textBox1.value = "";
+			textBox1.focus();
+			lbl1.style.color="white";
+			setTimeout (function()
+			{
+				lbl1.style.color="#607D8B";
+			 	console.log("interval");
+			}, 2000);
+	    }
+	 
 
- if (textBox1.value == NM)
- {
- 	alert("Félicitation vous avez trouvé nombre magique !!!!")
- }
+	
+
+
+	if (textBox1.value == NM)
+		{
+			lbl1.style.color="white";
+			lbl1.innerHTML = "Félicitation vous avez trouvé !!!!";
+			textBox1.value = "";
+			
+		}
+
+	
+
+	
 
 });
 
